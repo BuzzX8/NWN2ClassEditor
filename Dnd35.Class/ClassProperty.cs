dc87a286bf21cb5798dc45ba1b6033b28f2d48f5
Maxim Chasnik <maksym.c@sbtech.com>
2019-12-12 10:43:35 +0200

    wip

---
+++ Dnd35.Class/ClassProperty.cs
@@ -0,0 +1,18 @@
+﻿namespace Dnd35.Class
+{
+    public abstract class ClassProperty
+    {
+        protected ClassProperty(int id, int nameId = -1, int descriptionId = -1)
+        {
+            Id = id;
+            NameId = nameId;
+            DescriptionId = descriptionId;
+        }
+
+        public int Id { get; }
+
+        public int NameId { get; }
+
+        public int DescriptionId { get; }
+    }
+}
